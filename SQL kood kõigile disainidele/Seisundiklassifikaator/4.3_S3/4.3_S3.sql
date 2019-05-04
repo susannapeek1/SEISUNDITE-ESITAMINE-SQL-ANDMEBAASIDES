fa00ee7a37e6959858ed6170a3ccfcbf1609c50b
susannapeek1 <50082754+susannapeek1@users.noreply.github.com>
2019-05-04 22:03:07 +0300

    Delete 4.3_S3.sql

--- SQL kood kõigile disainidele/Seisundiklassifikaator/4.3_S3/4.3_S3.sql
+++
@@ -1,4 +0,0 @@
-SELECT tellimuse_seisundi_liik.nimetus AS hetkeseisund 
-FROM   tellimus 
-       INNER JOIN tellimuse_seisundi_liik ON tellimus.tellimuse_seisundi_liik_kood = tellimuse_seisundi_liik.tellimuse_seisundi_liik_kood 
-WHERE  tellimus.tellimuse_nr = 200000; 
