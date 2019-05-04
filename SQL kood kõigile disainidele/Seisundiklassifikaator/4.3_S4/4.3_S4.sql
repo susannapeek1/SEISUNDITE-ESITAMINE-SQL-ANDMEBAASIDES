9c305e989948648c8f37a4987a9eea72e91ec810
susannapeek1 <50082754+susannapeek1@users.noreply.github.com>
2019-05-04 22:03:15 +0300

    Delete 4.3_S4.sql

--- SQL kood kõigile disainidele/Seisundiklassifikaator/4.3_S4/4.3_S4.sql
+++
@@ -1,5 +0,0 @@
-SELECT tellimuse_seisundi_liik.nimetus AS seisund 
-FROM   tellimuse_seisundi_liik 
-WHERE  NOT EXISTS (SELECT * 
-                   FROM   tellimus 
-                   WHERE  tellimuse_seisundi_liik.tellimuse_seisundi_liik_kood = tellimus.tellimuse_seisundi_liik_kood); 
