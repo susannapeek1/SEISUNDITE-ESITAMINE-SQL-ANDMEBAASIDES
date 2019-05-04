ef44c7cfe363d4b2e1818cfe6419f7e62c731280
susannapeek1 <50082754+susannapeek1@users.noreply.github.com>
2019-05-04 22:08:08 +0300

    Delete 4.6_S2.sql

--- SQL kood kõigile disainidele/Vektorkodeerimine/4.6_S2/4.6_S2.sql
+++
@@ -1,5 +0,0 @@
-SELECT tellimus.tellimuse_nr, 
-       f_seisundi_dekodeerimine(seisund) AS hetkeseisund 
-FROM   tellimus 
-WHERE  f_seisundi_dekodeerimine(tellimus.seisund)<>'Ootel'
-ORDER  BY tellimus.tellimuse_nr; 
