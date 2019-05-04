de01cb9dccdffe390d8302ccb102a0e72f30544f
susannapeek1 <50082754+susannapeek1@users.noreply.github.com>
2019-05-04 22:05:09 +0300

    Delete 4.4_D.sql

--- SQL kood kõigile disainidele/Temporaalsed_veerud/4.4_D/4.4_D.sql
+++
@@ -1,5 +0,0 @@
-START TRANSACTION;
-ALTER TABLE tellimus DROP CONSTRAINT CHK_Tellimus_saab_olla_maksimaalselt_uhes_seisundis,
-                     ADD CONSTRAINT CHK_Tellimus_saab_olla_maksimaalselt_uhes_seisundis CHECK(num_nonnulls(ootel_aeg, tootlemisel_aeg, kohale_toimetatud_aeg, valja_saadetud_aeg)=1);
-ALTER TABLE tellimus DROP COLUMN tuhistatud_aeg;
-COMMIT;
